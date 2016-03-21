4e1a130104f733ebf69b5c5e408b1e8e155bb3f8
Ben Ebel <bge1234@gmail.com>
2016-03-21 14:49:51 -0600

    added map and filter

--- filter.js
+++ filter.js
@@ -1,11 +1,18 @@
 var arr = [1, 2, 3, 4];
 
 function filter(arr, cb) {
+  var output = [];
 
+  for (var i = 0; i < arr.length; i++) {
+    if(cb(arr[i], i, arr))
+      output.push(arr[i]);
+  }
+
+  return output;
 }
 
 var newArr = filter(arr, function (num, i, arr) {
   return num % 2 === 0;
 });
 
-console.log(newArr);
+console.log(newArr); // [2, 4]
--- map.js
+++ map.js
@@ -1,13 +1,17 @@
 var arr = [1, 2, 3, 4];
 
 function map(arr, cb) {
+  var output = [];
+
   for (var i = 0; i < arr.length; i++) {
-    cb(arr[i], i, arr);
+    output.push(cb(arr[i], i, arr));
   }
+
+  return output;
 }
 
 var newArray = map(arr, function (num, i, arr) {
   return num * 2;
 });
 
-console.log(newArray);
+console.log(newArray); // [2, 4, 6, 8]
